bcdc163a2d68d6a66380192c00fb6b72e700d85b
sai_govind <sai_govind@epam.com>
2024-04-05 09:50:04 +0530

    new h1 tag added

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-let a = document.getElementById('btn')
-
+let a = document.getElementById('btn');
+let b = document.getElementsByTagName('body');
+let h1=document.getElementById('heading');
+let c =0;
 
 a.onclick=()=>{
-  console.log("hello");
-  a.style.backgroundColor='red';
+   h1.textContent=`clicked ${++c}`;
 };
